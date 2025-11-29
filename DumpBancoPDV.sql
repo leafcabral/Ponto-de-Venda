3ce637c74a4918e0d1a8d10aa591fba1e4a90be6
ftfernandes <ftfernandes@gmail.com>
2025-11-29 13:15:13 -0300

    Adicionado script de criação da base e tabelas

---
+++ DumpBancoPDV.sql
@@ -0,0 +1,98 @@
+CREATE DATABASE  IF NOT EXISTS `exemplopdv` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `exemplopdv`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: exemplopdv
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `CPF` varchar(14) NOT NULL,
+  `nomeCliente` varchar(30) NOT NULL,
+  `emailCliente` varchar(50) NOT NULL,
+  PRIMARY KEY (`idCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `venda`
+--
+
+DROP TABLE IF EXISTS `venda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `venda` (
+  `idVenda` int NOT NULL AUTO_INCREMENT,
+  `dataVenda` date NOT NULL,
+  `valorVenda` float NOT NULL,
+  `idCliente` int NOT NULL,
+  PRIMARY KEY (`idVenda`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produto` (
+  `idProduto` int NOT NULL AUTO_INCREMENT,
+  `codProduto` int NOT NULL,
+  `nomeProduto` varchar(30) NOT NULL,
+  `qtdProduto` int NOT NULL,
+  `valorProduto` float NOT NULL,
+  PRIMARY KEY (`idProduto`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `itemvenda`
+--
+
+DROP TABLE IF EXISTS `itemvenda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itemvenda` (
+  `idItemVenda` int NOT NULL AUTO_INCREMENT,
+  `idVenda` int NOT NULL,
+  `idProduto` int NOT NULL,
+  `qtdProduto` int NOT NULL,
+  `vlrUnitario` float NOT NULL,
+  PRIMARY KEY (`idItemVenda`),
+  KEY `idVenda` (`idVenda`),
+  KEY `idProduto` (`idProduto`),
+  CONSTRAINT `itemvenda_ibfk_1` FOREIGN KEY (`idVenda`) REFERENCES `venda` (`idVenda`),
+  CONSTRAINT `itemvenda_ibfk_2` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idProduto`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Projeto Ponto de Venda - PDV em JAVA
+
 Código fonte referente às video-aulas do youtube:
-* Parte 1: https://www.youtube.com/watch?v=W3ZoblmSXJo&t=48s
+
+* Parte 1: https://www.youtube.com/watch?v=W3ZoblmSXJo\&t=48s
 * Parte 2: https://www.youtube.com/watch?v=OS18hLZnBwA
 
-## Pré-Requisitos 
+## Pré-Requisitos
+
 * IDE: Netbeans 16 - https://archive.apache.org/dist/netbeans/netbeans-installers/16/Apache-NetBeans-16-bin-windows-x64.exe
 * Mysql 8.0.XX Community Server - https://dev.mysql.com/downloads/file/?id=545896
-
+* Executar script DumpBancoPDV.sql no seu Mysql para criar a base exemplopdv e as tabelas
